de3b271ec177cad72b6959166ca9863fe0075af0
Christian Vilca <christian.vilcaq@gmail.com>
2018-10-30 22:41:50 -0500

    Prueba Real

--- test/chapter1/ScoreCollectionTest.java
+++ test/chapter1/ScoreCollectionTest.java
@@ -1,14 +1,22 @@
 package chapter1;
 
 import static org.junit.Assert.*;
-
+import static org.hamcrest.CoreMatchers.*; 
 import org.junit.Test;
 
 public class ScoreCollectionTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-
+	   public void answersArithmeticMeanOfTwoNumbers() {
+	      // Arrange
+	      ScoreCollection collection = new ScoreCollection();
+	      collection.add(() -> 5);
+	      collection.add(() -> 7);
+	      
+	      // Act
+	      int actualResult = collection.arithmeticMean();
+	      
+	      // Assert
+	      assertThat(actualResult, equalTo(6));
+	   }
 }
